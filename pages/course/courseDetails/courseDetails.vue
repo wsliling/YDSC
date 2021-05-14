d0ba7bebf25c5ff8d9c955229219569a9528ca2e
anye-18 <3092962017@qq.com>
2021-05-14 17:41:42 +0800

    0514

--- pages/course/courseDetails/courseDetails.vue
+++ pages/course/courseDetails/courseDetails.vue
@@ -26,7 +26,7 @@
 		<view class="line"></view>
 		<view class="con">
 			<view class="title">课程简介</view>
-			<view class="con1_1"><uParse :content="appcoursedetail.Content" /></view>
+			<view class="con1_1"><uParse v-if="appcoursedetail.Content" :content="appcoursedetail.Content" /></view>
 		</view>
 		<view class="line"></view>
 		<view class="time">
